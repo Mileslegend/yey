70efcafc7939de82593a57d37ceb0e34073ab284
Mileslegend <mileslegend7@gmail.com>
2024-11-29 15:58:05 +0000

    work

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function FeatureCard(props: {
+    title: string;
+    description: string;
+    children?: React.ReactNode;
+}) {
+    const { title, description, children } = props;
+    return (
+        <div>
+            <div>{children}</div>
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+}
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,6 @@
+import FeatureCard from "@/components/FeatureCard";
+import Tag from "@/components/Tag";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +12,52 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return <section className="py-24 ">
+        <div className="container">
+            <div className="flex justify-center">
+            <Tag>Features</Tag>
+            </div>
+            <h2 className="text-6xl font-medium text-center mt-6 ">Where power meets <span className="text-lime-400">simpicity</span></h2>
+            <div>
+                <FeatureCard title='Real-Time Collaboration' 
+                description='Work Together seamlessly with conflict-free team editing'
+                />
+                <FeatureCard title='Real-Time Collaboration' 
+                description='Work Together seamlessly with conflict-free team editing'
+                />
+                <FeatureCard title='Real-Time Collaboration' 
+                description='Work Together seamlessly with conflict-free team editing'
+                />
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Real-Time Collaboration</h3>
+                        <p>Work Together seamlessly with conflict-free team editing</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Interactive Prototyping</h3>
+                        <p>Engage your clients with prototypes that react to user actions </p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <h3>Ketboard Quick Actions</h3>
+                        <p>Powerful commands to help you create designs more quickly</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                {features.map((feature) => (
+                    <div key={feature}>
+                        <span></span>
+                        <span>{feature}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>;
 }
